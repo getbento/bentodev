0e0dedb717296509f4fbaadb6cda26998fc44203
djstein <djstein@ncsu.edu>
2017-04-03 17:57:40 -0400

    Base list, config, and start commands

--- bentodev/main.py
+++ bentodev/main.py
@@ -1,7 +1,38 @@
 import click
 
 
-@click.command()
+@click.group()
+@click.version_option()
 def cli():
-    """Example script."""
-    click.echo('Hello World!')
+    """BentoDev
+    Used to develop themes locally for BentoBox sites!
+    """
+
+######################################################
+
+
+@cli.command('config')
+@click.argument('arg')
+@click.option('--option', metavar='KN', default=10,
+              help='full option')
+def config(arg, option):
+    """Base configuration"""
+    click.echo('Testing config %s %s' % (arg, option))
+
+
+######################################################
+
+
+@cli.command('list')
+def list():
+    """List accounts and themes you have access too."""
+    click.echo('list list list')
+
+#####################################################
+
+
+@cli.command('start')
+@click.option('--site', default='seamores')
+def start(site):
+    """Begin running the development server"""
+    click.echo('runserver at %s.localdev.me:8000' % (site))
