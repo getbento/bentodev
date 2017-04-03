594d73f27e98ade14fbb7acc7c60a263b12914be
djstein <djstein@ncsu.edu>
2017-04-03 16:14:25 -0400

    inital project setup

---
+++ setup.py
@@ -0,0 +1,54 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='bentodev',
+
+    version='0.0.1',
+
+    description='BentoBox Local Development System',
+    long_description=long_description,
+
+    url='https://github.com/getbento/bentodev',
+
+    author='BentoBox',
+    author_email='dylan@getbento.com',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='bentobox bentodev',
+
+    entry_points={
+        'console_scripts': [
+            'sample=sample:main',
+        ],
+    },
+
+
+)
