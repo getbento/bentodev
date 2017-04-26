753b9fda153b2b7d3abc80dc758c9c7f92ccff32
djstein <djstein@ncsu.edu>
2017-04-26 13:10:24 -0400

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='bentodev',
-    version='0.0.5',
+    version='0.1.0',
     description='BentoBox Local Development System',
     long_description=long_description,
     url='https://github.com/getbento/bentodev',
