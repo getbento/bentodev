7e4dd6f32012fd20aceb3e3e00c25ffbba51f350
djstein <djstein@ncsu.edu>
2017-04-26 11:28:25 -0400

    complete install via pip

--- setup.py
+++ setup.py
@@ -1,24 +1,28 @@
-"""A setuptools based setup module.
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
-
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
 from os import path
+from codecs import open
+import sys
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    print("Please install setuptools: pip install setuptools")
+    sys.exit(1)
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
+
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as requirements_file:
+    install_requirements = requirements_file.read().splitlines()
+    if not install_requirements:
+        print("Unable to read requirements from the requirements.txt file.")
+        sys.exit(2)
+
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='bentodev',
-    version='0.0.1',
+    version='0.0.5',
     description='BentoBox Local Development System',
     long_description=long_description,
     url='https://github.com/getbento/bentodev',
@@ -26,33 +30,18 @@ setup(
     author_email='dylan@getbento.com',
     classifiers=[
         'Development Status :: 3 - Alpha',
+        'Environment :: Console',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'Programming Language :: Python :: 2.7',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
         'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Build Tools',
     ],
     keywords='bentobox bentodev',
-    install_requires=[
-        'Click==6.7',
-        'Flask==0.12.1',
-        'gitdb2==2.0.0',
-        'GitPython==2.1.3',
-        'itsdangerous==0.24',
-        'Jinja2==2.9.6',
-        'libsass==0.12.3',
-        'MarkupSafe==1.0',
-        'money==1.3.0',
-        'pip==9.0.1',
-        'python-dateutil==2.6.0',
-        'python-slugify==1.2.4',
-        'pytz==2017.2',
-        'requests==2.13.0',
-        'setuptools==28.8.0',
-        'six==1.10.0',
-        'smmap2==2.0.1',
-        'Unidecode==0.4.20',
-        'Werkzeug==0.12.1',
-    ],
+    packages=find_packages(),
+    package_data={'bentodev': ['base_config.json', 'setup_files/config.json']},
+    include_package_data=True,
+    install_requires=install_requirements,
     entry_points='''
         [console_scripts]
         bentodev=bentodev.main:cli
